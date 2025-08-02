3d1e761ef7f102b8d726bbf3f2413e5d5630491b
osnipassos <artful-morale0o@icloud.com>
2025-08-02 02:14:51 -0300

    api token

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-from fastapi import FastAPI
-from dotenv import load_dotenv
+from fastapi import FastAPI, Request, HTTPException
+from fastapi.responses import JSONResponse
 from carddav import get_contacts
-
-load_dotenv()
+import os
 
 app = FastAPI()
+API_TOKEN = os.getenv("API_TOKEN")
 
 @app.get("/contatos")
-def contatos():
+async def contatos(request: Request):
+    auth_header = request.headers.get("Authorization")
+
+    if not auth_header or auth_header != f"Bearer {API_TOKEN}":
+        raise HTTPException(status_code=401, detail="Unauthorized")
+
     contatos = get_contacts()
-    return {"contatos": contatos}
+    return JSONResponse(content={"contatos": contatos})
